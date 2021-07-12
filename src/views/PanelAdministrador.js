b9c65feabfe466cf955abf3768800e2c3a3d7edc
Emilioj17 <ejjelves@gmail.com>
2021-07-12 14:04:03 -0400

    Bienvenida y PrimerAdmin listos

--- src/views/PanelAdministrador.js
+++ src/views/PanelAdministrador.js
@@ -8,28 +8,24 @@ const PanelAdministrador = () => {
 
     const Inactivos = usuariosInactivos.map((usuario, index) => {
         return (
-            <tr>{usuario}</tr>
+            <li key={index}>{usuario}</li>
         )
     });
 
     const Activos = usuariosActivos.map((usuario, index) => {
         return (
-            <li>{usuario}</li>
+            <li key={index}>{usuario}</li>
         )
     });
 
-    return (
-        <div className="PanelAdministrador">
-            <div className="container">
-                <div className="d-flex justify-content-between align-items-center">
-                    <h2>Administracion de Usuarios</h2>
-                </div>
-                <div  className="col-12 col-md-6">
+    const DivUsuarios = () => {
+        return (
+            <div className="row">
+                <div className="col-6 col-md-3">
                     <table className="table">
                         <thead>
                             <tr>
                                 <th scope="col">Usuarios Inactivos</th>
-                                <th scope="col">Usuarios Activos</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -41,6 +37,19 @@ const PanelAdministrador = () => {
                                         </ul>
                                     </div>
                                 </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div  className="col-6 col-md-3">
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Usuarios Activos</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
                                 <td>
                                     <div>
                                         <ul>
@@ -52,6 +61,86 @@ const PanelAdministrador = () => {
                         </tbody>
                     </table>
                 </div>
+            </div>
+        )
+    }
+
+    const DivAdicional = () => {
+        const box1 = ["Configuración personal", "Detalles de empresa", "Reserva de calendario"];
+        const box2 = ["Módulos y campos", "Ciclos de venta", "Plantillas", "Copiar personalización", "Personalizar pantalla de inicio"];
+        const box3 = ["Administracion de Datos", "Importar/Exportar", "Copia de seguridad de datos", "Almacenamiento", "Papelera de reciclaje"];
+        const box4 = ["Espacio del Desarrollador", "API", "Formularios Web", "Cuenta Google"];
+
+        const Ebox1 = box1.map((elemento, index) => {
+            if (index == 0) {
+                return(<div key={index}><strong>{elemento}</strong></div>)
+            } else {
+                return (
+                    <li key={index}>{elemento}</li>
+                )
+            }
+        });
+
+        const Ebox2 = box2.map((elemento, index) => {
+            if (index == 0) {
+                return(<div key={index}><strong>{elemento}</strong></div>)
+            } else {
+                return (
+                    <li key={index}>{elemento}</li>
+                )
+            }
+        });
+
+        const Ebox3 = box3.map((elemento, index) => {
+            if (index == 0) {
+                return(<div key={index}><strong>{elemento}</strong></div>)
+            } else {
+                return (
+                    <li key={index}>{elemento}</li>
+                )
+            }
+        });
+
+        const Ebox4 = box4.map((elemento, index) => {
+            if (index == 0) {
+                return(<div key={index}><strong>{elemento}</strong></div>)
+            } else {
+                return (
+                    <li key={index}>{elemento}</li>
+                )
+            }
+        });
+
+        return (
+            <div>
+                <div className="row">
+                    <div className="box box1 rounded col-12 col-md-5 m-2">{Ebox1}</div>
+                    <div className="box box2 rounded col-12 col-md-5 m-2">{Ebox2}</div>
+                </div>
+                <div className="row">
+                    <div className="box box3 rounded col-12 col-md-5 m-2">{Ebox3}</div>
+                    <div className="box box4 rounded col-12 col-md-5 m-2">{Ebox4}</div>
+                </div>
+            </div>
+        )
+    }
+
+    return (
+        <div className="PanelAdministrador">
+            <div className="container">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h2>Administracion de Usuarios</h2>
+                </div>
+                <DivUsuarios />
+                <div className="col-12 col-md-6 d-flex justify-content-start">
+                    <button type="button" className="btn btn-success">Crear</button>
+                    <button type="button" className="btn btn-danger mx-3">Borrar</button>
+                    <button type="button" className="btn btn-warning">Modificar</button>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mt-5">
+                    <h2>Administracion de Adicional</h2>
+                </div>
+                <DivAdicional />
             </div>
       </div>
     );
