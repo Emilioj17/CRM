4299632d3ba6bfbaf6fed61808d854202d965ec9
c0venn <fabichidori2@gmail.com>
2021-07-15 19:24:52 +0000

    recuperar

--- src/views/Recuperar.js
+++ src/views/Recuperar.js
@@ -4,8 +4,38 @@ import '../style/Recuperar.css';
 function Recuperar() {
     return (
         <div className="container">
-            <h3 className="texto">Recupera Tu Clave</h3>
-        </div>
-    )
+            <div className="row">
+                <div className="col-md-12 col-md-offset-4">
+                    <div className="panel panel-default">
+                        <div className="panel-body">
+                            <div className="text-center">
+                                <h3><i className="fa fa-lock fa-4x"></i></h3>
+                                <h2 className="text-center">Forgot Password?</h2>
+                                <p>You can reset your password here.</p>
+                                <div className="panel-body">
+
+                                    <form id="register-form" role="form" autocomplete="off" className="form" method="post">
+
+                                        <div className="form-group">
+                                            <div className="input-group">
+                                                <span className="input-group-addon"><i className="glyphicon glyphicon-envelope color-blue"></i></span>
+                                                <input id="email" name="email" placeholder="email address" className="form-control" type="email"></input>
+                        </div>
+                                            </div>
+                                            <div className="form-group">
+                                                <input name="recover-submit" className="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit"></input>
+                      </div>
+
+                                                <input type="hidden" className="hide" name="token" id="token" value=""></input>
+                    </form>
+
+                                            </div>
+                                        </div>
+              </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    )
 }
-export default Recuperar;
+                    export default Recuperar;
