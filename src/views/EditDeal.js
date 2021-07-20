5f34e236e326ede624779ac9622421b1b5be8b82
Esteban Obilinovic <e.obilinovic@gmail.com>
2021-07-20 19:02:27 -0400

    editar trato

--- src/views/EditDeal.js
+++ src/views/EditDeal.js
@@ -24,74 +24,80 @@ function EditDeal(props) {
         setUserId(event.target.options[selectedIndex].getAttribute('user-id'));
     }
 
-    function handleSubmit(){
+    function handleSubmit() {
         editDeal(props.match.params.id, plan, duration, description, contactId, userId)
         history.push("/tratos");
     }
 
     useEffect(() => {
-        if(sessionStorage.getItem("token") === null){
+        actions.getDeal(props.match.params.id)
+        if (sessionStorage.getItem("token") === null) {
             history.push('/login')
         }
     }, []);
 
     return (
         <div className="container">
-            <div className="row">
-                <h1 className="my-5">Editar trato</h1>
-                <div className="d-flex flex-column align-items-center">
-                    <div className="col-6 my-3">
-                        <label htmlFor="plan" className="form-label">Plan</label>
-                        <select id="contact" className="form-select" onChange={(event) => setPlan(event.target.value)}>
-                            <option disabled selected>Selecciona una opción</option>
-                            <option >Plan 1</option>
-                            <option >Plan 2</option>
-                            <option >Plan 3</option>
-                            <option >Plan 4</option>
-                        </select>
+            {store.deal !== null && store.contacts && store.users !== null ?
+                <div className="row">
+                    <h1 className="my-5">Editar trato</h1>
+                    <div className="d-flex flex-column align-items-center">
+                        <div className="col-6 my-3">
+                            <label htmlFor="plan" className="form-label">Plan</label>
+                            <select defaultValue={store.deal.plan} id="contact" className="form-select" onChange={(event) => setPlan(event.target.value)}>
+                                <option disabled selected>Selecciona una opción</option>
+                                <option >Plan 1</option>
+                                <option >Plan 2</option>
+                                <option >Plan 3</option>
+                                <option >Plan 4</option>
+                            </select>
+                        </div>
+                        <div className="col-6 my-3">
+                            <label htmlFor="plan" className="form-label">Duración</label>
+                            <select defaultValue={store.deal.duration} id="contact" className="form-select" onChange={(event) => setDuration(event.target.value)}>
+                                <option disabled selected>Selecciona una opción</option>
+                                <option >Mensual</option>
+                                <option >Trimestral</option>
+                                <option >Semestral</option>
+                                <option >Anual</option>
+                            </select>
+                        </div>
+                        <div className="col-6 my-3">
+                            <label htmlFor="contact" className="form-label">Contacto</label>
+                            <select defaultValue={store.deal.contact_id + " " + (store.contacts.find(contact => contact.id === store.deal.contact_id).name) + " " + (store.contacts.find(contact => contact.id === store.deal.contact_id).last_name)} id="contact" className="form-select" onChange={onSelectContact}>
+                                <option disabled selected>Selecciona una opción</option>
+                                {contacts !== null && contacts.length > 0 ?
+                                    contacts.map((contact, i) =>
+                                        <option key={i} contact-id={contact.id}>{contact.id + ". " + contact.name + " " + contact.last_name}</option>)
+                                    : ""
+                                }
+                            </select>
+                        </div>
+                        <div className="col-6 my-3">
+                            <label htmlFor="contact" className="form-label">Usuario</label>
+                            <select defaultValue={store.deal.user_id + " " + (store.users.find(user => user.id === store.deal.user_id).name) + " " + (store.users.find(user => user.id === store.deal.user_id).last_name)} id="contact" className="form-select" onChange={onSelectUser}>
+                                <option disabled selected>Selecciona una opción</option>
+                                {users !== null && users.length > 0 ?
+                                    users.map((user, i) =>
+                                        <option key={i} user-id={user.id}>{user.id + ". " + user.name + " " + user.last_name}</option>)
+                                    : ""
+                                }
+                            </select>
+                        </div>
+                        <div className="col-6 my-3">
+                            <label htmlFor="description">Descripción</label>
+                            <textarea className="form-control" id="description" rows="3" onChange={(event) => setDescription(event.target.value)}></textarea>
+                        </div>
                     </div>
-                    <div className="col-6 my-3">
-                        <label htmlFor="plan" className="form-label">Duración</label>
-                        <select id="contact" className="form-select" onChange={(event) => setDuration(event.target.value)}>
-                            <option disabled selected>Selecciona una opción</option>
-                            <option >Mensual</option>
-                            <option >Trimestral</option>
-                            <option >Semestral</option>
-                            <option >Anual</option>
-                        </select>
-                    </div>
-                    <div className="col-6 my-3">
-                        <label htmlFor="contact" className="form-label">Contacto</label>
-                        <select id="contact" className="form-select" onChange={onSelectContact}>
-                            <option disabled selected>Selecciona una opción</option>
-                            {contacts !== null && contacts.length > 0 ?
-                                contacts.map((contact, i) =>
-                                    <option key={i} contact-id={contact.id}>{contact.id + ". " + contact.name + " " + contact.last_name}</option>)
-                                : ""
-                            }
-                        </select>
-                    </div>
-                    <div className="col-6 my-3">
-                        <label htmlFor="contact" className="form-label">Usuario</label>
-                        <select id="contact" className="form-select" onChange={onSelectUser}>
-                            <option disabled selected>Selecciona una opción</option>
-                            {users !== null && users.length > 0 ?
-                                users.map((user, i) =>
-                                    <option key={i} user-id={user.id}>{user.id + ". " + user.name + " " + user.last_name}</option>)
-                                : ""
-                            }
-                        </select>
-                    </div>
-                    <div className="col-6 my-3">
-                        <label htmlFor="description">Descripción</label>
-                        <textarea className="form-control" id="description" rows="3"onChange={(event) => setDescription(event.target.value)}></textarea>
-                    </div>
-                </div>
 
-                <div className="col-12 d-flex">
-                    <button className="col-2 btn btn-success my-5 ms-auto" onClick={handleSubmit}>Editar trato</button>
+                    <div className="col-12 d-flex">
+                        <button className="col-2 btn btn-success my-5 ms-auto" onClick={handleSubmit}>Editar trato</button>
+                    </div>
                 </div>
-            </div>
+                :
+                ""
+            }
+
         </div>
     )
 }
